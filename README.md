39561dc979031b738f93b384e0d5d80041ad7884
darryl <gundy@snakegully.net>
2021-06-07 20:15:50 +1000

    initial

---
+++ README.md
@@ -0,0 +1,51 @@
+
+barite is a library to access the backblaze b2 API.
+
+It is far from complete, but will have functions added as I need them. Contributions are
+welcome if you need a feature that isn't covered.
+
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     barite:
+       github: gundy818/barite
+   ```
+
+2. Run `shards install`
+
+
+## Usage
+
+```crystal
+require "barite"
+```
+
+TODO: Write usage instructions here
+
+
+## Development
+
+TODO: Write development instructions here
+
+
+## Contributing
+
+1. Fork it (<https://github.com/gundy818/barite/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+
+## Contributors
+
+- [darryl](https://github.com/gundy818) - creator and maintainer
+
+
+<!--- vim: textwidth=88
+ -->
+
---
+++ src/exception.cr
@@ -0,0 +1,34 @@
+# Exception definitions for the Barite module.
+#
+# In general, Barite will return these in preference to generic exceptions in places
+# where it can add some context and make the errors easier to interpret.
+#
+# If you want to trap all barite exceptions, you can:
+#
+#     begin
+#       something
+#     rescue BariteException
+#       ...
+#     end
+#
+
+
+module Barite
+  # Base exception.
+  class Exception < Exception
+  end
+
+  # Aomething went wrong with authentication.
+  # The specific message will tell you more. It could be a problem with credentials, or
+  # an error communicating with Backblazw.
+  class AuthenticationException < Barite::Exception
+  end
+
+  # Something was not found.
+  # You should get this if you try to retrieve or access something and there's a
+  # problem. Either the resource doesn't exist, or you don't have permissions, or there
+  # may have been a comms error.
+  class NotFoundException < Barite::Exception
+  end
+end
+
