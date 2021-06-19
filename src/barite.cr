ec68b044ab5bd7bc755787af03a3d13bb6016d08
darryl <gundy@snakegully.net>
2021-06-19 18:21:30 +1000

    Extract the Barite::VERSION constand from the shard metadata

--- src/barite.cr
+++ src/barite.cr
@@ -1,10 +1,10 @@
 #
 
-require "./b2/api"
-require "./b2/bucket"
-require "./b2/file"
 
 module Barite
-  VERSION = "0.1.0"
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+
 end
+
+require "./b2/**"
 
